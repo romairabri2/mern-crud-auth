b9bba547b2900c9567b4239c036b7b2fe92dfb1e
romairabri2 <romairabritom@gmail.com>
2023-12-09 18:43:59 -0400

    guardamos datos en la bdd

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 
-const app = express()
+import authRoutes from './routes/auth.routes.js';
+
+const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.use("/api",authRoutes);//todas las rutas comienzan con api
 
 export default app;
