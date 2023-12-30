984db8bfbd1a87fa4b7902555cdfcf44ba7902c8
romairabri2 <romairabritom@gmail.com>
2023-12-30 16:13:55 -0400

    Login finished

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -7,7 +7,7 @@ export const registerSchema = z.object({
     email: z.string({
         required_error: 'Email is required'
     }).email({
-        message: 'Invalid email'
+        message: 'Email is not valid'
     }),
     password: z.string({
         required_error: 'Password is required'
