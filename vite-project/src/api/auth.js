2a6d96e385efac25f8c54b4a2920b9caaa3b8d7e
romairabri2 <romairabritom@gmail.com>
2024-01-12 15:16:22 -0400

    correccion

--- vite-project/src/api/auth.js
+++ vite-project/src/api/auth.js
@@ -6,4 +6,4 @@ export const registerRequest = (user) => axios.post(`${API}/register`, user);
 
 export const loginRequest = (user) => axios.post(`${API}/login`, user);
 
-export const verityTokenRequest = () => axios.get('/verify');
+export const verityTokenRequest = () => axios.get(`/verify`);
